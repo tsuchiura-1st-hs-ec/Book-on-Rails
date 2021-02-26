eeda4da3cddddd3044ae7567b57e686ac26f8553
Yuzuki Arai <yudukikun5120@gmail.com>
2021-02-27 01:07:04 +0900

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
     mode: 'development',
     entry: './src/job.js.erb',
+    loader: 'babel-loader',
     output: {
         path: path.join(__dirname, '/dist'),
         filename: 'main.js'
